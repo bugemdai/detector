ca6cbdcc3c8d7da4d60a226f0b6737a25d72138b
bugemdai <37519790+bugemdai@users.noreply.github.com>
2025-03-13 11:11:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,16 @@
+# Define paths for various resources used in the project
+
+# Path to the directory containing video files
 PATH_VIDEO = 'source/Videos'
+
+# Path to the directory containing audio files
 PATH_AUDIO = 'source/Audio'
+
+# Path to the directory containing CSV files
 PATH_CSV = 'source/CSV'
+
+# Path to the dataset directory
 PATH_DATASET = 'source/dataset'
+
+# Main source directory path
 PATH_MAIN = 'source'
